73bbf4fd4ea4b853c22a9ab96adaa5bd072bac2f
Kazune Shidara <kazuneshidara@gmail.com>
2022-10-01 20:08:52 -0700

    refactoring

--- my-app/src/data.js
+++ my-app/src/data.js
@@ -13,12 +13,11 @@ It can be used for:
 1. **So much more!**
 
 ---
-
 Here's a division 👆`,
     },
-    { id: 2, body: `a` },
-    { id: 3, body: `b` },
-    { id: 4, body: `c` },
-    { id: 5, body: `d` },
-    { id: 6, body: `e` },
+        { id: 2, body: `a` },
+        { id: 3, body: `b` },
+        { id: 4, body: `c` },
+        { id: 5, body: `d` },
+        { id: 6, body: `e` },
 ]
